a95013ca8e1c00664814b16f5460fe2808eefd32
Jang-Je <jyhoo040912@gmail.com>
2024-09-14 23:42:22 +0900

    초기

--- src/main/java/com/example/demo/OrderApp.java
+++ src/main/java/com/example/demo/OrderApp.java
@@ -10,8 +10,8 @@ import com.example.demo.order.OrderServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        MemberService memberService = new MemberServiceImpl(memberRepository);
+        OrderService orderService = new OrderServiceImpl(memberRepository);
 
         Long memberID = 1L;
         Member member = new Member(memberID,"memberA", Grade.VIP);
---
+++ src/main/java/com/example/demo/discount/RateDiscountPolicy.java
@@ -0,0 +1,16 @@
+package com.example.demo.discount;
+
+import com.example.demo.member.Grade;
+import com.example.demo.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountParent = 10;
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP) {
+            return price * discountParent / 100;
+        }
+        return 0;
+    }
+}
